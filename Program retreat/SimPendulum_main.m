eefaf19399278981e6a3c89e32dea88786969050
WillaertJente <jente.willaert@kuleuven.be>
2021-11-29 13:49:23 +0100

    Import opensim params

--- Program retreat/SimPendulum_main.m
+++ Program retreat/SimPendulum_main.m
@@ -26,3 +26,10 @@ data_exp  = ImportExperimentalData(info, bool_plot, params_subject, dt_spline);
 bool_plot = 1;
 [data_exp.x0, data_exp.N_1] = PendulumPhases(data_exp, bool_plot);  
          
+%% Import OpenSim model parameters 
+% OpenSim 
+addpath('MuscleModel');
+muscles = {'rect_fem_','bifemlh_'}; 
+[params_OS] = ReadOpenSimParams(info, params_subject, muscles); 
+ 
+ 
